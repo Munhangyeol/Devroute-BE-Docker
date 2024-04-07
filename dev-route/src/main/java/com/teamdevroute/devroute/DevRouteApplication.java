9bcc20d67d0d8e837d883b0fe76558712bd29804
mereorfish <sungwon326@naver.com>
2024-04-07 14:17:17 +0900

    initial commit
    프로젝트 초기 파일 커밋

---
+++ dev-route/src/main/java/com/teamdevroute/devroute/DevRouteApplication.java
@@ -0,0 +1,13 @@
+package com.teamdevroute.devroute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevRouteApplication.class, args);
+	}
+
+}
---
+++ dev-route/src/test/java/com/teamdevroute/devroute/DevRouteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamdevroute.devroute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevRouteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
