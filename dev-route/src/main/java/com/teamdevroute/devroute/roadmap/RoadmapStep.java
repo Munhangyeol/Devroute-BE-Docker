5c8a47c7e620be93ae8d43baafe77086f77864ee
Munhangyeol <mhg10181018@gmail.com>
2024-07-11 14:14:28 +0900

    Refactor: Entity내의 GeneratedValue strategy 설정

--- dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStep.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStep.java
@@ -3,6 +3,7 @@ package com.teamdevroute.devroute.roadmap;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 
@@ -10,7 +11,7 @@ import lombok.Getter;
 @Getter
 public class RoadmapStep {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "roadmap_step_id")
     private Long id;
     private String development_field;
--- dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStepInfo.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStepInfo.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Converter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -23,7 +24,7 @@ import lombok.Getter;
 @Getter
 public class RoadmapStepInfo {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "roadmap_step_info_id")
     private Long id;
 
