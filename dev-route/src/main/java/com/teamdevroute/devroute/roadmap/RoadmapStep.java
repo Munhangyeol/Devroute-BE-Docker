3122b5a9afcf4365717bb3aa0ce358d2d5b42aaa
Munhangyeol <mhg10181018@gmail.com>
2024-07-11 12:23:10 +0900

    Modify Package Name

--- dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStep.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/roadmap/RoadmapStep.java
@@ -4,12 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
 import lombok.Getter;
 
 @Entity
 @Getter
-public class Roadmap_step {
+public class RoadmapStep {
     @Id
     @GeneratedValue
     @Column(name = "roadmap_step_id")
