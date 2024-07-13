f63f44840a8d0fc81c15505c202ca5b77f0c1e18
Munhangyeol <mhg10181018@gmail.com>
2024-07-13 14:23:45 +0900

    refactor:  VideoService내의 쿼리문들 상수화&fetch,save 관련 메서드public->private

--- dev-route/src/main/java/com/teamdevroute/devroute/video/VideoService.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/video/VideoService.java
@@ -1,6 +1,12 @@
 package com.teamdevroute.devroute.video;
 
 
+import static com.teamdevroute.devroute.video.constans.ApiConstans.QUERY_FRONT_KEY;
+import static com.teamdevroute.devroute.video.constans.ApiConstans.QUERY_FRONT_VALUE;
+import static com.teamdevroute.devroute.video.constans.ApiConstans.QUERY_MAX_RESULT;
+import static com.teamdevroute.devroute.video.constans.ApiConstans.YOUTUBE_API_URL_FRONT_VIDEOID;
+import static com.teamdevroute.devroute.video.constans.ApiConstans.YOUTUBE_API_URL_SEARCH;
+import static com.teamdevroute.devroute.video.constans.ApiConstans.YOUTUBE_API_URL_SEARCH;
 import static com.teamdevroute.devroute.video.enums.PlatformName.Youtube;
 
 import com.teamdevroute.devroute.video.dto.YouTubeApiResponse;
@@ -17,8 +23,6 @@ public class VideoService {
 
     @Value("${youtube.api.key}")
     private String youtubeApiKey;
-    @Value("${youtube.api.url}")
-    private String youtubeApiUrl;
 
     private String entireUrl;
 
@@ -32,23 +36,24 @@ public class VideoService {
         restTemplate = new RestTemplate();
         fetchAndSaveYoutubeVideos();
     }
-    public void fetchAndSaveYoutubeVideos() {
+   private void fetchAndSaveYoutubeVideos() {
         for (TechnologyStackName value : TechnologyStackName.values()) {
-            entireUrl = youtubeApiUrl + "part=snippet&type=video" + "&q=" +value + "&key=" + youtubeApiKey + "&maxResults=10";
+            entireUrl = YOUTUBE_API_URL_SEARCH + QUERY_FRONT_VALUE +value + QUERY_FRONT_KEY + youtubeApiKey + QUERY_MAX_RESULT;
             response = restTemplate.getForObject(entireUrl, YouTubeApiResponse.class);
             saveYoutubeVideo(response,value);
         }
     }
-    public void saveYoutubeVideo(YouTubeApiResponse response, TechnologyStackName teck_stack){
+    private void saveYoutubeVideo(YouTubeApiResponse response, TechnologyStackName teck_stack){
+        Long rank= Long.valueOf(0);
         for (YouTubeApiResponse.Item item : response.getItems()) {
             String videoId = item.getId().getVideoId();
-            String videoUrl = "https://www.youtube.com/watch?v=" + videoId;
+            String videoUrl = YOUTUBE_API_URL_FRONT_VIDEOID+videoId;
             String title = item.getSnippet().getTitle();
             String thumbnailUrl = item.getSnippet().getThumbnails().getDefault().getUrl();
             if (videoId != null && title != null && thumbnailUrl != null) {
 //                System.out.println(videoId + " " + videoUrl + " " + thumbnailUrl);
                 videoRepository.save(new YoutubeVideoDTO(videoUrl, title, thumbnailUrl).toEntity(
-                        String.valueOf(Youtube), String.valueOf(teck_stack)));
+                        String.valueOf(Youtube), String.valueOf(teck_stack), 0L,++rank));
             }
         }
 
