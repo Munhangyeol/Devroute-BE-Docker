4614dbbd18d4c263b4115ca9b17d1e9b965f800d
Munhangyeol <mhg10181018@gmail.com>
2024-07-15 11:31:36 +0900

    refactor & feat:  enum타입 technology stack name 관련 변경, application- secret 관련 ignore 추가

--- dev-route/src/main/java/com/teamdevroute/devroute/video/enums/TechnologyStackName.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/video/enums/TechnologyStackName.java
@@ -1,5 +1,5 @@
 package com.teamdevroute.devroute.video.enums;
 
 public enum TechnologyStackName {
-    htmlcss,파이썬,자바,자바스크립트,스프링,Nodejs,React,Vuejs,Angular,
+    htmlcss,python,java,javascript,spring,Nodejs,React,Vuejs,Angular,
 }
