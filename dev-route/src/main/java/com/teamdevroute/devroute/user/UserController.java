1256fbe548e5dd046a72cbf0728427a8543252fe
maark1106 <mark1106@naver.com>
2024-07-22 22:41:10 +0900

    feat : kakao 로그인 Controller 구현

--- dev-route/src/main/java/com/teamdevroute/devroute/user/UserController.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/UserController.java
@@ -1,5 +1,6 @@
 package com.teamdevroute.devroute.user;
 
+import com.teamdevroute.devroute.global.auth.ELoginProvider;
 import com.teamdevroute.devroute.user.dto.UserCreateRequest;
 import com.teamdevroute.devroute.user.dto.UserCreateResponse;
 import jakarta.servlet.http.Cookie;
@@ -7,13 +8,14 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
-@Controller
+@RestController
 public class UserController {
     private final UserService userService;
 
@@ -40,4 +42,8 @@ public class UserController {
         response.addCookie(cookie);
     }
 
+    @GetMapping("/auth/kakao")
+    public void kakaoCallback(@RequestParam("code") String code) {
+        String accessToken = userService.getAccessToken(code, ELoginProvider.KAKAO);
+    }
 }
