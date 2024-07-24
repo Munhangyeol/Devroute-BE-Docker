44109fa37f90c800df59919de8bc27d0314d9465
(sungwon) <(sungwon326@naver.com)>
2024-07-24 16:20:20 +0900

    [#25]feat: 상위 30개 기업의 채용공고 크롤링

--- dev-route/src/main/java/com/teamdevroute/devroute/crawling/CompanyCrawlingService.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/crawling/CompanyCrawlingService.java
@@ -2,6 +2,7 @@ package com.teamdevroute.devroute.crawling;
 
 import com.teamdevroute.devroute.company.Company;
 import com.teamdevroute.devroute.company.CompanyRepository;
+import com.teamdevroute.devroute.crawling.dto.CrawledCompanyDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,19 +16,19 @@ public class CompanyCrawlingService {
         this.companyRepository = companyRepository;
     }
 
-    public void createCompany(
-            List<String> enterpriseNames,
-            List<String> enterpriseSalaries,
-            List<String> enterpriseGrades,
-            List<String> enterpriseLogos
-    ) {
+    public void createCompany(CrawledCompanyDto crawledCompanyDto) {
+        List<String> enterpriseNames = crawledCompanyDto.getEnterpriseNames();
+        List<String> enterpriseSalaries = crawledCompanyDto.getEnterpriseSalaries();
+        List<String> enterpriseGrades = crawledCompanyDto.getEnterpriseGrades();
+        List<String> enterpriseLogo = crawledCompanyDto.getEnterpriseLogo();
+
         for(int i=0;i<enterpriseNames.size();i++) {
             companyRepository.save(
                     Company.builder()
                     .name(enterpriseNames.get(i))
                     .averageSalary(enterpriseSalaries.get(i))
                     .grade(Double.parseDouble(enterpriseGrades.get(i)))
-                    .logoUrl(enterpriseLogos.get(i))
+                    .logoUrl(enterpriseLogo.get(i))
                     .build()
             );
         }
