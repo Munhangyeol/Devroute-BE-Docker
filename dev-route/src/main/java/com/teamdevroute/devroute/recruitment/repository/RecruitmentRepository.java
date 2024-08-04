dbb1f50155a6fc219110d953c15be31c77fdfa42
(sungwon) <(sungwon326@naver.com)>
2024-08-04 15:12:11 +0900

    [#59] feat: 직종별 채용공고 검색
    - type을 쿼리 파라미터로 받아 각 직종의 채용공고 검색
    - type이 이상할 경우, IllegalArgumentException 발생

--- dev-route/src/main/java/com/teamdevroute/devroute/recruitment/repository/RecruitmentRepository.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/recruitment/repository/RecruitmentRepository.java
@@ -1,8 +1,18 @@
 package com.teamdevroute.devroute.recruitment.repository;
 
 import com.teamdevroute.devroute.recruitment.domain.Recruitment;
+import com.teamdevroute.devroute.user.enums.DevelopField;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
     boolean existsByUrl(String url);
+
+    @Query("select r from Recruitment r where r.developField = :developField")
+    List<Recruitment> findByDevelopField(DevelopField developField);
 }
--- dev-route/src/main/java/com/teamdevroute/devroute/user/enums/DevelopField.java
+++ dev-route/src/main/java/com/teamdevroute/devroute/user/enums/DevelopField.java
@@ -1,5 +1,21 @@
 package com.teamdevroute.devroute.user.enums;
 
 public enum DevelopField {
-    BACKEND, FRONTEND, MOBILE, AI, DATA_SCIENCE, NONE
+    BACKEND,
+    FRONTEND,
+    MOBILE,
+    AI,
+    DATA_SCIENCE,
+    NONE;
+
+    public static DevelopField toEnum(String type) {
+        return switch (type) {
+            case "backend" -> DevelopField.BACKEND;
+            case "frontend" -> DevelopField.FRONTEND;
+            case "ai" -> DevelopField.AI;
+            case "mobile" -> DevelopField.MOBILE;
+            case "datascience" -> DevelopField.DATA_SCIENCE;
+            default -> DevelopField.NONE;
+        };
+    }
 }
